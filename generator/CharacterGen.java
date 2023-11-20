fa634d26e59f9507870e8c8e88295d54ab6fde20
Max Paulo <maxpauloteixeira@gmail.com>
2023-11-19 23:34:24 -0300

    Começando o Character generator

---
+++ generator/CharacterGen.java
@@ -0,0 +1,35 @@
+package generator;
+import dice.Dice;
+
+public class CharacterGen {
+    
+    String[][] mascNames;
+    String[][] femNames;
+    String[] appearance;
+    String[] personality;
+
+    CharacterGen(){
+        mascNames = new String[][] {
+            {"Carlos", "Mateus", "Teodoro"},
+            {"Neto", "Tomás", "Reginaldo"},
+            {"Tadeu", "Joaquim", "José"},
+            {"Ângelo", "Heitor", "Alceu"},
+            {"Douglas", "Geraldo", "Lorenzo"},
+            {"Eric", "Alfredo", "Diogo"},
+            {"Alex", "Samuel", "Cícero"},
+            {"Álvaro", "Adriano", "Érico"},
+            {"Reinaldo", "Igor", "Guilherme"},
+            {"Sérgio", "Osvaldo", "Evandro"},
+            {"Cássio", "Hugo", "Vicente"},
+            {"Júlio", "Valdomiro", "Benedito"},
+            {"Nicolas", "Conrado", "Nelson"},
+            {"Armando", "Sebastião", "Renan"},
+            {"Cauã", "Jorge", "Davi"},
+            {"Vítor", "Roberto", "Arthur"},
+            {"Mauro", "Arnoldo", "Saulo"},
+            {"Henrique", "Breno", "Antenor"},
+            {"Fabrício", "Renato", "Túlio"},
+            {"Giovane", "Aurélio", "Osmar"},
+        };
+    }
+}
