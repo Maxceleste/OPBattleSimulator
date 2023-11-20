a58402ebfa548b91b3034696cb880cdc746d6364
Max Paulo <maxpauloteixeira@gmail.com>
2023-11-19 23:05:39 -0300

    Adicionando mais opçoes no método dos dados

--- dice/Dice.java
+++ dice/Dice.java
@@ -4,12 +4,14 @@ import java.util.Random;
 
 public class Dice {
     
-    public static int rollD20(int numberOfDices, int advantages){
+    public static int rollD20(int numberOfDices, int advantages, boolean showText){
         int[] values;
         int finalValue;
 
-        System.out.println("Valor do atributo: " + numberOfDices);
-        System.out.println("Bônus de +" + advantages);
+        if (showText){
+            System.out.println("Valor do atributo: " + numberOfDices);
+            System.out.println("Bônus de +" + advantages);
+        }
 
         if (numberOfDices  <= 0){
             values = new int[2];
@@ -21,9 +23,9 @@ public class Dice {
 
                 if (values[i] < finalValue) finalValue = values[i];
 
-                System.out.println("Dado nº" + i + ": "  + values[i]);
+                if (showText) System.out.println("Dado nº" + i + ": "  + values[i]);
             }
-            System.out.println("Resultado: " + (finalValue + advantages));
+            if (showText) System.out.println("Resultado: " + (finalValue + advantages));
             return finalValue + advantages;
 
         } else {
@@ -36,19 +38,19 @@ public class Dice {
 
                 if (values[i] > finalValue) finalValue = values[i];
 
-                System.out.println("Dado nº" + i + ": "  + values[i]);
+                if (showText) System.out.println("Dado nº" + i + ": "  + values[i]);
             }
-            System.out.println("Resultado: " + (finalValue + advantages));
+            if (showText) System.out.println("Resultado: " + (finalValue + advantages));
             return finalValue + advantages;
 
         }
     }
 
-    public static int rollDice(int sides, int numberOfDices){
+    public static int rollDice(int sides, boolean showText, int numberOfDices){
         int counter = 0;
         int finalResult = 0;
 
-        System.out.println("Rolando dado de " + sides + " lados...");
+        if (showText) System.out.println("Rolando dado de " + sides + " lados...");
 
         while (counter < numberOfDices){
 
@@ -58,20 +60,20 @@ public class Dice {
             
             counter++;
 
-            System.out.println("Dado " + counter + ": " + result);
+            if (showText) System.out.println("Dado " + counter + ": " + result);
         }
 
-        System.out.println("Resultado final: " + finalResult);
+        if (showText) System.out.println("Resultado final: " + finalResult);
         return finalResult;
     }
-    public static int rollDice(int sides){
+    public static int rollDice(int sides, boolean showText){
 
-        System.out.println("Rolando dado de " + sides + " lados...");
+        if (showText) System.out.println("Rolando dado de " + sides + " lados...");
         
         Random random = new Random();
         int result = random.nextInt(1, sides + 1);
 
-        System.out.println("Resultado: " + result);
+        if (showText) System.out.println("Resultado: " + result);
         return result;
     }
 
--- main/Main.java
+++ main/Main.java
@@ -7,28 +7,30 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Incializando OPRPG Battle...");
 
+        boolean showText = true;
+
         System.out.println("***************************************");
-        Dice.rollD20(3, 5);
+        Dice.rollD20(3, 5, showText);
         System.out.println("***************************************");
-        Dice.rollDice(12);
+        Dice.rollDice(12, showText);
         System.out.println("***************************************");
-        Dice.rollDice(12, 2);
+        Dice.rollDice(12, showText, 2);
         System.out.println("***************************************");
-        Dice.rollDice(10);
+        Dice.rollDice(10, showText);
         System.out.println("***************************************");
-        Dice.rollDice(10, 2);
+        Dice.rollDice(10, showText, 2);
         System.out.println("***************************************");
-        Dice.rollDice(8);
+        Dice.rollDice(8, showText);
         System.out.println("***************************************");
-        Dice.rollDice(8, 2);
+        Dice.rollDice(8, showText, 2);
         System.out.println("***************************************");
-        Dice.rollDice(6);
+        Dice.rollDice(6, showText);
         System.out.println("***************************************");
-        Dice.rollDice(6, 2);
+        Dice.rollDice(6, showText, 2);
         System.out.println("***************************************");
-        Dice.rollDice(4);
+        Dice.rollDice(4, showText);
         System.out.println("***************************************");
-        Dice.rollDice(4, 2);
+        Dice.rollDice(4, showText, 2);
         System.out.println("***************************************");
 
     }
