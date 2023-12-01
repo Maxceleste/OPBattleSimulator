f0a077a292f56aae9a44cc3ec13952fda58c6546
Max Paulo <maxpauloteixeira@gmail.com>
2023-11-30 21:50:05 -0300

    Começando o gerador de classe

--- generator/CharacterGen.java
+++ generator/CharacterGen.java
@@ -3,8 +3,49 @@ import dice.Dice;
 import java.util.ArrayList;
 
 public class CharacterGen {
+
+    Data data = new Data();
+
     
-    String[][] mascNames = new String[][] {
+    int[] attributes = new int[5]; // AGI, INT, VIG, PRE, FOR
+    String fullName = "";
+    ArrayList<Object> origin = new ArrayList<>();
+    String charClass = "";
+    ArrayList<String> skills = new ArrayList<>();
+
+
+    public String[] genOrigin(){
+        int resultado = Dice.rollDice(24, false) - 1;
+        String[] finalOrigin = Data.origins[resultado];
+
+        origin.add(finalOrigin[0]);
+        origin.add(finalOrigin[3]);
+
+        skills.add(finalOrigin[1]);
+        skills.add(finalOrigin[2]);
+
+        return finalOrigin;
+    } 
+
+    public void genClass(){ //Colocar pra retornar String
+        switch((String) origin.get(0)){
+            case "Acadêmico":
+                data.classData[0][1] = 3;
+                break;
+            case "Agente de Saúde":
+            //-------------
+                break;
+            case "Amnésico":
+            //-------------
+                break;
+        }
+    }
+    
+}
+
+
+class Data{
+    static String[][] mascNames = new String[][] {
         {"Carlos", "Mateus", "Teodoro"},
         {"Neto", "Tomás", "Reginaldo"},
         {"Tadeu", "Joaquim", "José"},
@@ -26,10 +67,10 @@ public class CharacterGen {
         {"Fabrício", "Renato", "Túlio"},
         {"Giovane", "Aurélio", "Osmar"}
     };
-    String[][] femNames;
-    String[] appearance;
-    String[] personality;
-    String[][] origins = new String[][] { // Origem, Pericia1, Pericia2, Poder
+    static String[][] femNames;
+    static String[] appearance;
+    static String[] personality;
+    static String[][] origins = new String[][] { // Origem, Pericia1, Pericia2, Poder
         {"Acadêmico", "Ciências", "Investigação", "Saber é Poder"},
         {"Agente de Saúde", "Intuição", "Medicina", "Técnica Medicinal"},
         {"Amnésico", null, null, "Vislumbres do Passado"},
@@ -56,18 +97,9 @@ public class CharacterGen {
         {"Universitário", "Atualidades", "Investigação", "Empenho"}
     };
 
-    int[] attributes = new int[5]; // AGI, INT, VIG, PRE, FOR
-    String fullName = "";
-    ArrayList<Object> origin = new ArrayList<>();
-
-
-    public String[] genOrigin(){
-        int resultado = Dice.rollDice(24, false) - 1;
-        return origins[resultado];
-    } 
-
-
-    
-
-    
+    Object[][] classData = new Object[][] {
+        {"Combatente", (Integer) 1},
+        {"Especialista", (Integer) 1},
+        {"Ocultista", (Integer) 1},
+    };
 }
